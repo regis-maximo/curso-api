820113c7f970ac1e3ae8db93d965630d81a2f7a5
Regis Maximo <regishallowed@gmail.com>
2020-10-18 20:01:51 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlunoCreateComponent } from './escola/aluno-create/aluno-create.component';
+import { AlunoDeleteComponent } from './escola/aluno-delete/aluno-delete.component';
+import { AlunoReadComponent } from './escola/aluno-read/aluno-read.component';
+import { AlunoUpdateComponent } from './escola/aluno-update/aluno-update.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'aluno-create', component: AlunoCreateComponent },
+  { path: 'aluno-read', component: AlunoReadComponent },
+  { path: 'aluno-update/:id', component: AlunoUpdateComponent },
+  { path: 'aluno-delete/:id', component: AlunoDeleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { AlunoCreateComponent } from './escola/aluno-create/aluno-create.component';
+import { AlunoReadComponent } from './escola/aluno-read/aluno-read.component';
+import { AlunoUpdateComponent } from './escola/aluno-update/aluno-update.component';
+import { AlunoDeleteComponent } from './escola/aluno-delete/aluno-delete.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    AlunoCreateComponent,
+    AlunoReadComponent,
+    AlunoUpdateComponent,
+    AlunoDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    LayoutModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
